1cefb37bd877123411be53ea3e9414290591474e
d11416146 <d11416146@gs.takming.edu.tw>
2026-03-10 15:31:05 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace class_ch12_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
